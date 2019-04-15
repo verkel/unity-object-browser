425110b9f35f4065ec28d113d72fea5c60b573c3
Jaakko Lindvall <verkel@iki.fi>
2019-04-15 03:52:34 +0300

    SingletonList: displays all singletons derived from a class that has a static Instance property

---
+++ SingletonList.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace DebugObjectBrowser {
+	public class SingletonsList : IShowAsList {
+		private readonly Type baseType;
+		private readonly string instancePropertyName;
+
+		private List<PropertyInfo> instanceProperties;
+
+		public SingletonsList(Type baseType, string instancePropertyName = "Instance") {
+			this.instancePropertyName = instancePropertyName;
+			this.baseType = baseType;
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() {
+			return SingletonsEnumerator();
+		}
+
+		private IEnumerator<object> SingletonsEnumerator() {
+			foreach (var property in GetInstanceProperties()) {
+				object value;
+				try {
+					value = property.GetValue(null);
+				}
+				catch (Exception e) {
+					value = e.GetType().Name;
+				}
+				yield return value;
+			}
+		}
+
+		private IEnumerable<PropertyInfo> GetInstanceProperties() {
+			if (instanceProperties == null) {
+				instanceProperties = baseType.Assembly.GetTypes()
+					.Select(type => type.BaseType)
+					.Where(bt => bt != null && bt.IsGenericType && bt.GetGenericTypeDefinition() == baseType)
+					.Select(bt => bt.GetProperty(instancePropertyName))
+					.ToList();
+			}
+			return instanceProperties;
+		}
+
+		public override string ToString() {
+			return string.Format("Singletons ({0})", baseType.Name);
+		}
+	}
+}
